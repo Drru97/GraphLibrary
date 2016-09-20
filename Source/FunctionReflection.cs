60a732681b7a02ba2bbf783849e244c77cee1b5b
Oleg Dutsyak <oleg.dystak@gmail.com>
2016-09-20 17:52:12 +0300

    Documented and optimized

--- Source/FunctionReflection.cs
+++ Source/FunctionReflection.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.CodeDom.Compiler;
+using System.IO;
 using System.Reflection;
 using Microsoft.CSharp;
 
 namespace GraphLibrary.Source
 {
+    /// <summary>
+    /// This class is used to convert specifed as
+    /// string functions into lambda expressions
+    /// </summary>
     public class FunctionReflection
     {
+        /// <value> Sets functions as a string </value>
         public string Function { get; set; }
         private readonly CSharpCodeProvider _codeProvider;
         private readonly CompilerParameters _compilerParameters;
@@ -26,25 +32,57 @@ namespace GraphLibrary.Source
                             }
                         }";
 
+        /// <summary>
+        /// Creates an instance of this class
+        /// with specified comliler parameters
+        /// </summary>
+        /// <param name="function"> Mathematical function in string view </param>
         public FunctionReflection(string function)
         {
             Function = function;
             _codeProvider = new CSharpCodeProvider();
-            _compilerParameters = new CompilerParameters { GenerateInMemory = true, ReferencedAssemblies = { "System.dll" } };
+            _compilerParameters = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                ReferencedAssemblies = { "System.dll" }
+            };
             Compile();
         }
 
+        /// <summary>
+        /// This method is used to compile source code
+        /// from strings with specified parameters
+        /// </summary>
+        /// <exception cref="FileNotFoundException"> Throws when 
+        /// given incorrect function and compiler cannot compile assembly</exception>
         private void Compile()
         {
             _compilerResults = _codeProvider.CompileAssemblyFromSource(_compilerParameters,
                 BeginString + Function + EndString);
-            var cls = _compilerResults.CompiledAssembly.GetType("Test.FunctionCreator");
-            var method = cls.GetMethod("CreateFunction", BindingFlags.Static | BindingFlags.Public);
-            _delegate = (method.Invoke(null, null) as Delegate);
+            try
+            {
+                var cls = _compilerResults.CompiledAssembly.GetType("Test.FunctionCreator");
+                var method = cls.GetMethod("CreateFunction", BindingFlags.Static | BindingFlags.Public);
+                _delegate = (method.Invoke(null, null) as Delegate);
+            }
+            catch (FileNotFoundException)
+            {
+                // ignored
+            }
         }
 
+        /// <summary>
+        /// This method is used to dynamic invoke lambda
+        /// expression as a result of compilation
+        /// </summary>
+        /// <param name="x"> Function argument x </param>
+        /// <returns> Function value y </returns>
+        /// <exception cref="ArgumentNullException"> Throws when
+        /// given incorrect function </exception>
         public double InvokeMethod(double x)
         {
+            if (_delegate == null)
+                throw new ArgumentNullException(nameof(x), "Incorrect function");
             return (double)_delegate.DynamicInvoke(x);
         }
     }
