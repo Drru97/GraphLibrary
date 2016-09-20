e38e2abdc275749bac1e965e79fdf966b23905f8
Oleg Dutsyak <oleg.dystak@gmail.com>
2016-09-20 19:10:24 +0300

    Documentation

--- Source/FunctionOperations.cs
+++ Source/FunctionOperations.cs
@@ -7,15 +7,34 @@ using System.Data;
 
 namespace GraphLibrary.Source
 {
+    /// <summary>
+    /// Class which contains information about
+    /// function and processing methods
+    /// </summary>
     public class FunctionOperations
     {
+        /// <value> Minimum value of function </value>
         public double LeftLimit { get; set; }
+
+        /// <value> Maximum value of function</value>
         public double RightLimit { get; set; }
+
+        /// <value> Number of points which will be used in charting </value>
         public int NumberOfPoints { get; set; }
+
+        /// <value> Array of function arguments x </value>
         public double[] X { get; set; }
+
+        /// <value> Array of function values y </value>
         public double[] Y { get; set; }
+
+        /// <value> Sets mathematical function as a string </value>
         public string Function { get; set; }
 
+        /// <summary>
+        /// Method is used to get a function values y
+        /// from function arguments x
+        /// </summary>
         private void Tabulation()
         {
             double h = (RightLimit - LeftLimit) / NumberOfPoints;
@@ -29,12 +48,18 @@ namespace GraphLibrary.Source
             Y[Y.Length - 1] = reflection.InvokeMethod(X[X.Length - 1]);
         }
 
+        /// <summary>
+        /// Creates an instance of this class
+        /// with preset mathematical function and
+        /// default parameters and tabulate it
+        /// </summary>
+        /// <param name="function"> Mathematical function in string view </param>
         public FunctionOperations(string function)
         {
             Function = function;
             LeftLimit = -5;
             RightLimit = 5;
-            NumberOfPoints = 100;
+            NumberOfPoints = 1000;
             X = new double[NumberOfPoints];
             Y = new double[NumberOfPoints];
             Tabulation();
