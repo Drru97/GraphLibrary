98660018e39768e45549acdb9ea4f29f6a10bbf8
Oleg Dutsyak <oleg.dystak@gmail.com>
2016-09-20 16:58:45 +0300

    Optimization

--- Source/FunctionOperations.cs
+++ Source/FunctionOperations.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.CSharp;
-using System.CodeDom.Compiler;
 using System.Data;
-using System.Reflection;
 
 namespace GraphLibrary.Source
 {
@@ -19,43 +16,17 @@ namespace GraphLibrary.Source
         public double[] Y { get; set; }
         public string Function { get; set; }
 
-        private const string beginString = @"using System;
-                                       namespace Test
-                                       {
-                                            public delegate double Calc(double x);
-                                            public static class FunctionCreator
-                                            {
-                                                public static Calc CreateFunction()
-                                                {
-                                                    return x=>";
-
-        private const string endString = @";
-                                 }
-                            }
-                        }";
-
         private void Tabulation()
         {
             double h = (RightLimit - LeftLimit) / NumberOfPoints;
             X[0] = LeftLimit;
+            FunctionReflection reflection = new FunctionReflection(Function);
             for (int i = 0; i < NumberOfPoints - 1; i++)
             {
-                Y[i] = BuildFunction(X[i]);
+                Y[i] = reflection.InvokeMethod(X[i]);
                 X[i + 1] = X[i] + h;
             }
-            Y[Y.Length - 1] = BuildFunction(X[X.Length - 1]);
-        }
-
-        private double BuildFunction(double x)
-        {
-            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
-            CompilerParameters compilerParameters = new CompilerParameters { GenerateInMemory = true, ReferencedAssemblies = { "System.dll" } };
-            CompilerResults compilerResults = codeProvider.CompileAssemblyFromSource(compilerParameters,
-                beginString + Function + endString);
-            var cls = compilerResults.CompiledAssembly.GetType("Test.FunctionCreator");
-            var method = cls.GetMethod("CreateFunction", BindingFlags.Static | BindingFlags.Public);
-            var del = (method.Invoke(null, null) as Delegate);
-            return (double)del.DynamicInvoke(x);
+            Y[Y.Length - 1] = reflection.InvokeMethod(X[X.Length - 1]);
         }
 
         public FunctionOperations(string function)
@@ -63,7 +34,7 @@ namespace GraphLibrary.Source
             Function = function;
             LeftLimit = -5;
             RightLimit = 5;
-            NumberOfPoints = 50;
+            NumberOfPoints = 100;
             X = new double[NumberOfPoints];
             Y = new double[NumberOfPoints];
             Tabulation();
---
+++ Source/FunctionReflection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using Microsoft.CSharp;
+
+namespace GraphLibrary.Source
+{
+    public class FunctionReflection
+    {
+        public string Function { get; set; }
+        private readonly CSharpCodeProvider _codeProvider;
+        private readonly CompilerParameters _compilerParameters;
+        private CompilerResults _compilerResults;
+        private Delegate _delegate;
+        private const string BeginString = @"using System;
+                                            namespace Test
+                                             {
+                                                   public delegate double Calc(double x);
+                                                   public static class FunctionCreator
+                                                   {
+                                                       public static Calc CreateFunction()
+                                                       {
+                                                          return x=>";
+        private const string EndString = @";
+                                 }
+                            }
+                        }";
+
+        public FunctionReflection(string function)
+        {
+            Function = function;
+            _codeProvider = new CSharpCodeProvider();
+            _compilerParameters = new CompilerParameters { GenerateInMemory = true, ReferencedAssemblies = { "System.dll" } };
+            Compile();
+        }
+
+        private void Compile()
+        {
+            _compilerResults = _codeProvider.CompileAssemblyFromSource(_compilerParameters,
+                BeginString + Function + EndString);
+            var cls = _compilerResults.CompiledAssembly.GetType("Test.FunctionCreator");
+            var method = cls.GetMethod("CreateFunction", BindingFlags.Static | BindingFlags.Public);
+            _delegate = (method.Invoke(null, null) as Delegate);
+        }
+
+        public double InvokeMethod(double x)
+        {
+            return (double)_delegate.DynamicInvoke(x);
+        }
+    }
+}
